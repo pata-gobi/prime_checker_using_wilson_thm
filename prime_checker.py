ab7b8f78b4b50ccc564c94b073883f02c1aa247b
pata-gobi <168411155+pata-gobi@users.noreply.github.com>
2024-06-11 15:58:26 +0530

    Add files via upload

---
+++ prime_checker.py
@@ -0,0 +1,10 @@
+def fact(n):
+    prod = 1
+    for i in range(2,n+1):
+        prod=prod*i
+    return prod
+
+if (fact(j:=int(input("enter:")) -1 )-1) %j :
+    print("prime")
+else:
+    print("not prime")
